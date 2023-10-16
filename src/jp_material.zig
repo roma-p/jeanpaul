34139230b48accc2c2c1e3768da266019561a2e3
roma_p <pelle.romain@gmail.com>
2023-10-16 20:34:08 +0200

    feat(JpMaterial) now has object like interface, same as JpObject.

--- src/jp_material.zig
+++ src/jp_material.zig
@@ -3,40 +3,58 @@ const allocator = std.heap.page_allocator;
 const types = @import("types.zig");
 const jp_color = @import("jp_color.zig");
 
-pub const JpMaterial = struct {
-    mat: *Material = undefined,
+// ==== Constants ============================================================
+
+pub const MaterialTypeId = enum {
+    Lambert,
 };
 
 const Material = union(enum) {
     Lambert: *MatLambert,
 };
 
+// ==== JpMaterial ===========================================================
+
+pub const JpMaterial = struct {
+    mat: *Material = undefined,
+    name: []const u8 = undefined,
+
+    const Self = @This();
+
+    pub fn new(name: []const u8, material_type_id: MaterialTypeId) !*Self {
+        var jpmat = try allocator.create(Self);
+        var mat = try allocator.create(Material);
+        switch (material_type_id) {
+            .Lambert => {
+                var actual_mat = try allocator.create(MatLambert);
+                actual_mat.* = MatLambert{};
+                mat.* = Material{ .Lambert = actual_mat };
+            },
+        }
+        jpmat.mat = mat;
+        jpmat.name = name;
+        return jpmat;
+    }
+
+    pub fn delete(self: *Self) void {
+        switch (self.mat.*) {
+            .Lambert => allocator.destroy(self.mat.Lambert),
+        }
+        allocator.destroy(self.mat);
+        allocator.destroy(self);
+    }
+};
+
+// ==== Material Definition ==================================================
+
 pub const MatLambert = struct {
     diffuse: jp_color.JpColor = jp_color.JP_COLOR_DEFAULT,
 };
 
-pub fn create_material_lambert() !*JpMaterial {
-    var lambert = try allocator.create(MatLambert);
-    lambert.* = MatLambert{};
-    var mat_wrapper = try allocator.create(Material);
-    mat_wrapper.* = Material{ .Lambert = lambert };
-    var mat = try allocator.create(JpMaterial);
-    mat.* = JpMaterial{
-        .mat = mat_wrapper,
-    };
-    return mat;
-}
-
-pub fn delete_material(material: *JpMaterial) void {
-    switch (material.mat.*) {
-        .Lambert => allocator.destroy(material.mat.Lambert),
-    }
-    allocator.destroy(material.mat);
-    allocator.destroy(material);
-}
+// ==== HELPERS ==============================================================
 
 pub fn create_default_colored_material(color: jp_color.JpColor) !*JpMaterial {
-    var material = try create_material_lambert();
+    var material = try JpMaterial.new("default", MaterialTypeId.Lambert);
     material.mat.Lambert.diffuse = color;
     return material;
 }
